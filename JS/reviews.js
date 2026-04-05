f2eca775b776b6c6ebd5467bdf7c345d97f47711
Vivekmurugulla2004 <155022092+Vivekmurugulla2004@users.noreply.github.com>
2026-04-04 20:00:47 -0400

    Add The Amazing Spider-Man (2012) review

--- JS/reviews.js
+++ JS/reviews.js
@@ -1,9 +1,9 @@
 const recentReviews = [
+    { title: 'The Amazing Spider-Man (2012)',                        poster: 'Images/Review/Poster/tasm poster.jpg',   url: 'Reviews/tasm.html'   },
     { title: 'Star Wars: Episode III — Revenge of the Sith (2005)', poster: 'Images/Review/Poster/sw3 poster.jpg',    url: 'Reviews/sw3.html'    },
     { title: '500 Days of Summer (2009)',                            poster: 'Images/Review/Poster/500dos poster.png', url: 'Reviews/500dos.html' },
     { title: 'Spider-Man 2 (2004)',                                  poster: 'Images/Review/Poster/sm2 poster.jpg',    url: 'Reviews/sm2.html'    },
     { title: 'Spider-Man 3 (2007)',                                  poster: 'Images/Review/Poster/sm3 poster.webp',   url: 'Reviews/sm3.html'    },
-    { title: 'Batman v. Superman: Dawn of Justice (2016)',           poster: 'Images/Review/Poster/bvs poster.jpg',    url: 'Reviews/bvs.html'    },
 ];
 
 const container = document.getElementById('reviews-container');
